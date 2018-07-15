151faf57726a01f57cf653f988ad6c6bcd896eb4
jamespjbennett <jamespjbennett@hotmail.com>
2018-07-15 16:35:21 +0100

    minute by minute logic

--- js/data_v2.js
+++ js/data_v2.js
@@ -1,45 +1,190 @@
-var teamsV2 = {
+Array.prototype.sum = function (prop) {
+    var total = 0
+    for ( var i = 0, _len = this.length; i < _len; i++ ) {
+        total += this[i][prop]
+    }
+    return total
+}
+
+teamsV2 = {
  	'Manchester United' : {
-	 	'attack' : 85,
-		'defence' :6,
-		'goals' : 4.5,
-		'opponents' : [],
 		'players': {
-			'Lukaku' : 9.9,
-			'Rashford' : 5.5,
-			'Sanchez' : 5.5,
-			'Mata' : 3.0,
-			'Lingard' : 4.0,
-			'Martial' : 5.7,
-			'Fred' : 1.2,
-			'Matic' : 0.8,
-			'Pogba' : 2.9,
-			'Smalling' : 0.7,
-			'Fellaini' : 2.5,
-			'Bailly' : 0.7,
-			'Valencia' : 0.3,
-			'Lindelof' : 0.3,
-			'Jones' : 0.3,
-			'Young' : 0.9,
+			'Lukaku' : {
+				'name' : 'Lukaku',
+				'scoreChance' : 9.9,
+				'defence': 40,
+				'attack': 82,
+			},
+			'Rashford' : {
+				'name' : 'Rashford',
+				'scoreChance' : 5.5,
+				'defence': 20,
+				'attack': 73,
+			},
+			'Sanchez' : {
+				'name' : 'Sanchez',
+				'scoreChance' : 5.5,
+				'defence': 29,
+				'attack': 77,
+			},
+			'Mata' : {
+				'name' : 'Mata',
+				'scoreChance' : 3.0,
+				'defence': 22,
+				'attack': 69,
+			},
+			'Lingard' : {
+				'name' : 'Lingard',
+				'scoreChance' : 4.0,
+				'defence': 35,
+				'attack': 73,
+			},
+			'Martial' : {
+				'name' : 'Martial',
+				'scoreChance' : 5.7,
+				'defence': 22,
+				'attack': 77,
+			},
+			'Fred' : {
+				'name' : 'Fred',
+				'scoreChance' : 1.2,
+				'defence': 50,
+				'attack': 60,
+			},
+			'Matic' : {
+				'name' : 'Matic',
+				'scoreChance' : 0.8,
+				'defence': 65,
+				'attack': 60,
+			},
+			'Pogba' : {
+				'name' : 'Pogba',
+				'scoreChance' : 2.9,
+				'defence': 45,
+				'attack': 70,
+			},
+			'Smalling' : {
+				'name' : 'Smalling',
+				'scoreChance' : 0.7,
+				'defence': 73,
+				'attack': 30,
+			},
+			'Fellaini' : {
+				'name' : 'Fellaini',
+				'scoreChance' : 2.5,
+				'defence': 58,
+				'attack': 65,
+			},
+			'Bailly' : {
+				'name' : 'Bailly',
+				'scoreChance' : 0.7,
+				'defence': 85,
+				'attack': 30,
+			},
+			'Valencia' : {
+				'name' : 'Valencia',
+				'scoreChance' : 0.3,
+				'defence': 70,
+				'attack': 40,
+			},
+			'Lindelof' : {
+				'name' : 'Lindelof',
+				'scoreChance' : 0.3,
+				'defence': 80,
+				'attack': 25,
+			},
+			'Jones' : {
+				'name' : 'Jones',
+				'scoreChance' : 0.3,
+				'defence': 75,
+				'attack': 25,
+			},
+			'Young' : {
+				'name' : 'Young',
+				'scoreChance' : 0.9,
+				'defence': 70,
+				'attack': 50,
+			},
 		}
  	},
  	'Bournemouth' : {
-	 	'attack' : 79,
-		'defence' :4.1,
-		'goals' : 4.1,
-		'opponents' : [],
 		'players' : {
-			'King' : 9.9,
-			'Wilson' : 9.9,
-			'Stanislas' : 5.0,
-			'Gosling' : 2.9,
-			'Fraser' : 5.0,
-			'Daniels' : 1.2,
-			'Defoe' : 4.0,
-			'Arter' : 0.7,
-			'Ibe' : 2.0,
-			'Ake' : 0.5,
-
+			'King' : {
+				'name' : 'King',
+				'scoreChance' : 9.9,
+				'defence': 25,
+				'attack': 68,
+			},
+			'Wilson' : {
+				'name' : 'Wilson',
+				'scoreChance' : 9.9,
+				'defence': 20,
+				'attack': 68,
+			},
+			'Stanislas' : {
+				'name' : 'Stanislas',
+				'scoreChance' : 5.0,
+				'defence': 20,
+				'attack': 58,
+			},
+			'Gosling' : {
+				'name' : 'Gosling',
+				'scoreChance' : 2.9,
+				'defence': 35,
+				'attack': 44,
+			},
+			'Fraser' : {
+				'name' : 'Fraser',
+				'scoreChance' : 5.0,
+				'defence': 15,
+				'attack': 43,
+			},
+			'Daniels' : {
+				'name' : 'Daniels',
+				'scoreChance' : 1.2,
+				'defence': 55,
+				'attack': 38,
+			},
+			'Defoe' : {
+				'name' : 'Defoe',
+				'scoreChance' : 4.0,
+				'defence': 5,
+				'attack': 69,
+			},
+			'Arter' : {
+				'name' : 'Arter',
+				'scoreChance' : 0.7,
+				'defence': 40,
+				'attack': 55,
+			},
+			'Ibe' : {
+				'name' : 'Ibe',
+				'scoreChance' : 2.0,
+				'defence': 15,
+				'attack': 60,
+			},
+			'Ake' : {
+				'name' : 'Ake',
+				'scoreChance' : 0.5,
+				'defence': 65,
+				'attack': 27,
+			},
+			'Cook' : {
+				'name' : 'Cook',
+				'scoreChance' : 0.5,
+				'defence': 60,
+				'attack': 25,
+			},
+			'Francis' : {
+				'name' : 'Francis',
+				'scoreChance' : 0.5,
+				'defence': 55,
+				'attack': 26,
+			},
 		}
- 	},
-}
+ 	}
+};
+
+
+
+
--- js/game_logic_v2.js
+++ js/game_logic_v2.js
@@ -1,1 +1,165 @@
-game_logic_v2.js
+Array.prototype.sum = function (prop) {
+    var total = 0
+    for ( var i = 0, _len = this.length; i < _len; i++ ) {
+        total += this[i][prop]
+    }
+    return total
+};
+
+
+function likelyShotsArray(teamShots){
+	var shots_array = [];
+	for(x=0;x<=10;x++){
+		if(x>0){
+			if(teamShots-x < 0){
+	     		break;
+	   		}else{
+	    		var iterationNumber = 20 - (x*2);
+	    		for(y=1;y<=iterationNumber;y++){
+	    			shots_array.push(teamShots + x);
+	        		shots_array.push(teamShots - x);
+		  		};
+	   		};
+	 	}else{
+	    	for(y=1;y<=20;y++){
+	    		shots_array.push(teamShots);
+		  	};  
+	 	};
+	};
+	return shots_array;
+};
+
+// TESTING THE 'PICKED' TEAMS
+
+// 1 MANAGER HAS PICKED THE TEAM
+var team1 = {
+	'name': 'Manchester United',
+	'players': [
+		teamsV2['Manchester United']['players']['Bailly'],
+		teamsV2['Manchester United']['players']['Valencia'],
+		teamsV2['Manchester United']['players']['Lindelof'],
+		teamsV2['Manchester United']['players']['Young'],
+		teamsV2['Manchester United']['players']['Matic'],
+		teamsV2['Manchester United']['players']['Fred'],
+		teamsV2['Manchester United']['players']['Pogba'],
+		teamsV2['Manchester United']['players']['Lukaku'],
+		teamsV2['Manchester United']['players']['Sanchez'],
+		teamsV2['Manchester United']['players']['Martial'],
+	],
+};
+
+var team2 = {
+	'name': 'Bournemouth',
+	'players' : [
+		teamsV2['Bournemouth']['players']['Francis'],
+		teamsV2['Bournemouth']['players']['Ake'],
+		teamsV2['Bournemouth']['players']['Daniels'],
+		teamsV2['Bournemouth']['players']['Cook'],
+		teamsV2['Bournemouth']['players']['Arter'],
+		teamsV2['Bournemouth']['players']['Fraser'],
+		teamsV2['Bournemouth']['players']['Gosling'],
+		teamsV2['Bournemouth']['players']['Ibe'],
+		teamsV2['Bournemouth']['players']['King'],
+		teamsV2['Bournemouth']['players']['Wilson'],
+	],
+};
+
+// TEAMS ATTACK AND DEFENCE RATINGS ARE CALCULATED BASED ON THE PLAYERS
+team1['attack'] = team1["players"].sum("attack");
+team1['defence'] = team1["players"].sum("defence");
+team2['attack'] = team2["players"].sum("attack");
+team2['defence'] = team2["players"].sum("defence");
+
+// TRIAL AND ERROR CALCULATION TO GET HOW MANY SHOTS THE TEAM WILL HAVE
+var team1MaxShots = team1["players"].sum("attack") / 24;
+var team2ShotRemoval = team2['defence'] / 35;
+var team2MaxShots = team2["players"].sum("attack") / 24;
+var team1ShotRemoval = team1['defence'] / 35;
+
+var team1ShotsAverage = Math.round(team1MaxShots - team2ShotRemoval);
+var team2ShotsAverage = Math.round(team2MaxShots - team1ShotRemoval);
+
+var team1ShotPossibilties = likelyShotsArray(team1ShotsAverage).sort();
+var team2ShotPossibilties = likelyShotsArray(team2ShotsAverage).sort();
+
+var team1ShotFinal = shuffle(team1ShotPossibilties)[0];
+var team2ShotFinal = shuffle(team2ShotPossibilties)[0];
+
+var gameStats = {
+	'team1Score': 0,
+	'team1Scorers':{},
+	'team2Score': 0,
+	'team2Scorers':{},
+	'team1Shots': 0,
+	'team2Shots': 0,
+	'minutesPlayed': 0,
+};
+
+var minuteStats = {};
+for(i=1; i<=90;i++){
+	minuteStats[i] = {
+		'team1Shots': 0,
+		'team2Shots': 0
+	};
+};
+
+for(i=1;i<=team1ShotFinal;i++){
+	var minute = shuffle(Object.keys(minuteStats))[0];
+	minuteStats[minute]["team1Shots"] = minuteStats[minute]["team1Shots"] + 1;
+};
+
+for(i=1;i<=team2ShotFinal;i++){
+	var minute = shuffle(Object.keys(minuteStats))[0];
+	minuteStats[minute]["team2Shots"] = minuteStats[minute]["team2Shots"] + 1;
+}
+
+// PLAY GAME
+
+var timer = 0;
+
+var intervalID = setInterval(function () {
+	timer+=1;
+	gameStats['minutesPlayed'] == timer;
+   if(minuteStats[timer]['team1Shots'] != 0){
+ 	  incrementShotsAndCalculateScoreLikelihood('MANU',minuteStats[timer]['team1Shots'], timer, gameStats, 'team1Score', 'team1Scorers', 'team1Shots');
+   }else{
+   };
+   if(minuteStats[timer]['team2Shots'] != 0){
+ 	  incrementShotsAndCalculateScoreLikelihood('BOURNEMOUTH', minuteStats[timer]['team2Shots'], timer, gameStats, 'team2Score', 'team2Scorers', 'team2Shots');
+   };  
+   if(minuteStats[timer]['team2Shots'] + minuteStats[timer]['team1Shots'] == 0){
+		console.log(timer + ' minutes: MANU ' + gameStats['team1Score'] + ' BOURN ' + gameStats['team2Score']);
+   };
+   if (timer === 90) {
+       window.clearInterval(intervalID);
+		console.log('score is MANU ' + gameStats['team1Score'] + ' BOURN ' + gameStats['team2Score'] );
+   }
+}, 100);
+
+
+function incrementShotsAndCalculateScoreLikelihood(teamName, shotCount, minute, gameStats, score, scorers, shots){
+	for(z=1;z<=shotCount;z++){
+		gameStats[shots] += 1;
+		if(isAGoal()){
+			gameStats[score] +=1;
+			gameStats[scorers][minute] = "Scorer name";
+			console.log('GOAL FOR '+teamName+'!! score is MANU ' + gameStats['team1Score'] + ' BOURN ' + gameStats['team2Score'] );
+		}else{
+			console.log('shot for '+teamName+'. score is MANU ' + gameStats['team1Score'] + ' BOURN ' + gameStats['team2Score'] );
+		}
+	};
+};
+
+function isAGoal(){
+	return shuffle([true, false, false, false])[0];
+};
+
+
+
+
+
+
+
+
+
+
