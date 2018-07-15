801d4a7c31bf1a8a730eb59fc2d6c098a49f3ad0
jamespjbennett <jamespjbennett@hotmail.com>
2018-07-15 19:03:39 +0100

    team list select

--- js/select-menu.js
+++ js/select-menu.js
@@ -1,1 +1,11 @@
 const choices = new Choices('.team-selector');
+
+function populatePlayers(){
+	var team = event.target.value;
+	var playerlist = Object.keys(teamsV2[event.target.value]['players']);
+	var selecter = document.getElementById(event.target.id + '-list');
+	selecter.innerHTML = "";	
+	for(i=0;i<playerlist.length;i++){
+		selecter.insertAdjacentHTML('beforeend', '<li>'+playerlist[i]+'</li>');
+	}
+};
