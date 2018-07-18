e0f95f463bb58142f768072e3d9ec5963a00c6e0
jamespjbennett <jamespjbennett@hotmail.com>
2018-07-18 21:21:28 +0100

    making interactive version of the gameplay

--- js/game_logic_v2.js
+++ js/game_logic_v2.js
@@ -29,89 +29,82 @@ function likelyShotsArray(teamShots){
 	return shots_array;
 };
 
-// TESTING THE 'PICKED' TEAMS
 
-// 1 MANAGER HAS PICKED THE TEAM
-var team1 = {
-	'name': 'Manchester United',
-	'players': [
-		teamsV2['Manchester United']['players']['Bailly'],
-		teamsV2['Manchester United']['players']['Valencia'],
-		teamsV2['Manchester United']['players']['Lindelof'],
-		teamsV2['Manchester United']['players']['Young'],
-		teamsV2['Manchester United']['players']['Matic'],
-		teamsV2['Manchester United']['players']['Fred'],
-		teamsV2['Manchester United']['players']['Pogba'],
-		teamsV2['Manchester United']['players']['Lukaku'],
-		teamsV2['Manchester United']['players']['Sanchez'],
-		teamsV2['Manchester United']['players']['Martial'],
-	],
+function playDetailedGamed(){
+	var homeTeamName = document.getElementById('home').value
+	var awayTeamName = document.getElementById('away').value
+	var team1 = generateTeam(homeTeamName, 'home-team');
+	var team2 = generateTeam(awayTeamName, 'away-team');
+	var team1Shots = calculateShots(team1);
+	var team2Shots = calculateShots(team2);
 };
 
-var team2 = {
-	'name': 'Bournemouth',
-	'players' : [
-		teamsV2['Bournemouth']['players']['Francis'],
-		teamsV2['Bournemouth']['players']['Ake'],
-		teamsV2['Bournemouth']['players']['Daniels'],
-		teamsV2['Bournemouth']['players']['Cook'],
-		teamsV2['Bournemouth']['players']['Arter'],
-		teamsV2['Bournemouth']['players']['Fraser'],
-		teamsV2['Bournemouth']['players']['Gosling'],
-		teamsV2['Bournemouth']['players']['Ibe'],
-		teamsV2['Bournemouth']['players']['King'],
-		teamsV2['Bournemouth']['players']['Wilson'],
-	],
+
+function generateTeam(team, selector){
+	var object = {}
+	object['name'] = team;
+	object['players'] = [];
+	var parentDiv = document.querySelector("."+selector);
+	var playerSelection = parentDiv.querySelectorAll('.position-blob');
+	for(i=0;i<playerSelection.length;i++){
+		var player = playerSelection[1].getAttribute('data-player');
+		object['players'].push(teamsV2[team]['players'][player])
+	};
+	return object;
 };
+
+
 
 // TEAMS ATTACK AND DEFENCE RATINGS ARE CALCULATED BASED ON THE PLAYERS
-team1['attack'] = team1["players"].sum("attack");
-team1['defence'] = team1["players"].sum("defence");
-team2['attack'] = team2["players"].sum("attack");
-team2['defence'] = team2["players"].sum("defence");
+// function calculateDefenceAndAttackRatings(object){
+// 	object['attack'] = object["players"].sum("attack");
+// 	object['defence'] = object["players"].sum("defence");	
+// }
 
-// TRIAL AND ERROR CALCULATION TO GET HOW MANY SHOTS THE TEAM WILL HAVE
-var team1MaxShots = team1["players"].sum("attack") / 24;
-var team2ShotRemoval = team2['defence'] / 35;
-var team2MaxShots = team2["players"].sum("attack") / 24;
-var team1ShotRemoval = team1['defence'] / 35;
+// // TRIAL AND ERROR CALCULATION TO GET HOW MANY SHOTS THE TEAM WILL HAVE
 
-var team1ShotsAverage = Math.round(team1MaxShots - team2ShotRemoval);
-var team2ShotsAverage = Math.round(team2MaxShots - team1ShotRemoval);
 
-var team1ShotPossibilties = likelyShotsArray(team1ShotsAverage).sort();
-var team2ShotPossibilties = likelyShotsArray(team2ShotsAverage).sort();
-
-var team1ShotFinal = shuffle(team1ShotPossibilties)[0];
-var team2ShotFinal = shuffle(team2ShotPossibilties)[0];
-
-var gameStats = {
-	'team1Score': 0,
-	'team1Scorers':{},
-	'team2Score': 0,
-	'team2Scorers':{},
-	'team1Shots': 0,
-	'team2Shots': 0,
-	'minutesPlayed': 0,
+function calculateShots(object){
+	// TEAMS ATTACK AND DEFENCE RATINGS ARE CALCULATED BASED ON THE PLAYERS
+	object['attack'] = object["players"].sum("attack");
+	object['defence'] = object["players"].sum("defence");	
+	// CALCULATION TO GET HOW MANY SHOTS THE TEAM WILL HAVE
+	var objectMaxShots = object["players"].sum("attack") / 24;
+	var objectShotRemoval = object['defence'] / 35;
+	var objectShotsAverage = Math.round(objectMaxShots - objectShotRemoval);
+	var objectShotPossibilties = likelyShotsArray(objectShotsAverage).sort();
+	var objectShotFinal = shuffle(objectShotPossibilties)[0];
+	return objectShotFinal
 };
 
-var minuteStats = {};
-for(i=1; i<=90;i++){
-	minuteStats[i] = {
-		'team1Shots': 0,
-		'team2Shots': 0
-	};
-};
 
-for(i=1;i<=team1ShotFinal;i++){
-	var minute = shuffle(Object.keys(minuteStats))[0];
-	minuteStats[minute]["team1Shots"] = minuteStats[minute]["team1Shots"] + 1;
-};
+// var gameStats = {
+// 	'team1Score': 0,
+// 	'team1Scorers':{},
+// 	'team2Score': 0,
+// 	'team2Scorers':{},
+// 	'team1Shots': 0,
+// 	'team2Shots': 0,
+// 	'minutesPlayed': 0,
+// };
 
-for(i=1;i<=team2ShotFinal;i++){
-	var minute = shuffle(Object.keys(minuteStats))[0];
-	minuteStats[minute]["team2Shots"] = minuteStats[minute]["team2Shots"] + 1;
-}
+// var minuteStats = {};
+// for(i=1; i<=90;i++){
+// 	minuteStats[i] = {
+// 		'team1Shots': 0,
+// 		'team2Shots': 0
+// 	};
+// };
+
+// for(i=1;i<=team1ShotFinal;i++){
+// 	var minute = shuffle(Object.keys(minuteStats))[0];
+// 	minuteStats[minute]["team1Shots"] = minuteStats[minute]["team1Shots"] + 1;
+// };
+
+// for(i=1;i<=team2ShotFinal;i++){
+// 	var minute = shuffle(Object.keys(minuteStats))[0];
+// 	minuteStats[minute]["team2Shots"] = minuteStats[minute]["team2Shots"] + 1;
+// }
 
 // PLAY GAME
 
@@ -137,19 +130,23 @@ var timer = 0;
 // }, 100);
 
 
-function incrementShotsAndCalculateScoreLikelihood(teamName, shotCount, minute, gameStats, score, scorers, shots){
-	for(z=1;z<=shotCount;z++){
-		gameStats[shots] += 1;
-		if(isAGoal()){
-			gameStats[score] +=1;
-			gameStats[scorers][minute] = scorerName(teamName);
-			console.log('GOAL FOR '+teamName+'!! score is MANU ' + gameStats['team1Score'] + ' BOURN ' + gameStats['team2Score'] );
-		}else{
-			console.log('shot for '+teamName+'. score is MANU ' + gameStats['team1Score'] + ' BOURN ' + gameStats['team2Score'] );
-		}
-	};
-};
+// function incrementShotsAndCalculateScoreLikelihood(teamName, shotCount, minute, gameStats, score, scorers, shots){
+// 	for(z=1;z<=shotCount;z++){
+// 		gameStats[shots] += 1;
+// 		if(isAGoal()){
+// 			gameStats[score] +=1;
+// 			gameStats[scorers][minute] = scorerName(teamName);
+// 			console.log('GOAL FOR '+teamName+'!! score is MANU ' + gameStats['team1Score'] + ' BOURN ' + gameStats['team2Score'] );
+// 		}else{
+// 			console.log('shot for '+teamName+'. score is MANU ' + gameStats['team1Score'] + ' BOURN ' + gameStats['team2Score'] );
+// 		}
+// 	};
+// };
 
-function isAGoal(){
-	return shuffle([true, false, false, false])[0];
+// function isAGoal(){
+// 	return shuffle([true, false, false, false])[0];
+// };
+
+document.getElementById('play-game').onclick = function(e){
+  playDetailedGamed();
 };
